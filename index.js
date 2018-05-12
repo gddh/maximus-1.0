30233d106978872f2d24a455d7621b29cbdf0cfe
Derrick <d.lin168@berkeley.edu>
2018-05-12 16:08:31 -0700

    tryiing one more time

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const request = require('request');
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 'use strict';
 
@@ -7,7 +7,7 @@ const request = require('request');
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()), // creates express http server
+  app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
