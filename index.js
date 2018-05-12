6b7bc151434492f79599148daef1d55e989b79c6
Derrick <d.lin168@berkeley.edu>
2018-05-12 13:35:37 -0700

    moved back to original index.js

--- index.js
+++ index.js
@@ -1,66 +1,64 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
-app.set('port', (process.env.PORT || 5000))
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
 
-// Process application/json
-app.use(bodyParser.json())
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
 
-// Index route
-app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
-})
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 
-app.post('/webhook/', function (req, res) {
-        let messaging_events = req.body.entry[0].messaging
-        for (let i = 0; i < messaging_events.length; i++) {
-                    let event = req.body.entry[0].messaging[i]
-            let sender = event.sender.id
-            if (event.message && event.message.text) {
-                            let text = event.message.text
-                sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-            }
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
     }
-    res.sendStatus(200)
-})
-
-const token = "<PAGE_ACCESS_TOKEN>"
-
-function sendTextMessage(sender, text) {
-        let messageData = { text:text }
-            request({
-                        url: 'https://graph.facebook.com/v2.6/me/messages',
-                        qs: {access_token:token},
-                        method: 'POST',
-                        json: {
-                                        recipient: {id:sender},
-                            message: messageData,
-                        }
-                }, function(error, response, body) {
-                            if (error) {
-                                            console.log('Error sending messages: ', error)
-                        } else if (response.body.error) {
-                                        console.log('Error: ', response.body.error)
-                        }
-                                })
-}
+  }
+});
