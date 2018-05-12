093a90a671da7331765b92ccb4af893c8e1e8498
Derrick <d.lin168@berkeley.edu>
2018-05-12 11:27:17 -0700

    added start script

--- myapp/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express' 4.16.3)
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello Word!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
