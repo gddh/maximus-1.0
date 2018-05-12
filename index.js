ff0b93c3b37022f02fd06836187ba557ef131fdf
Derrick <d.lin168@berkeley.edu>
2018-05-12 12:10:52 -0700

    changed index back

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 'use strict';
 
 // Imports dependencies and set up http server
 const
+  express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
