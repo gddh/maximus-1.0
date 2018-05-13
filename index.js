ec052e6400813c2679cbe7c99475c6001a8bc36c
Derrick <d.lin168@berkeley.edu>
2018-05-12 17:12:09 -0700

    added attachmets

--- index.js
+++ index.js
@@ -90,6 +90,10 @@ function handleMessage(sender_psid, received_message) {
         response = {
             "text": `You sent the message: "${received_message.text}". Now send me an image!`
         }
+    } else if (received_message.attachments) {
+        
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
     }
 
     // Sends the response message
