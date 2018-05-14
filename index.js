6dc8768ffde9aa31af08beed8aef2e68646527d1
Derrick <d.lin168@berkeley.edu>
2018-05-13 19:15:42 -0700

    using .message

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ function handleMessage(sender_psid, received_message) {
 
     // Check if the message contains text
     if (received_message.text) {
+        console.log("wit.message:");
         console.log(wit.message(received_message.text));
         response = { "text": `You sent the message: "${received_message.text}". Now send me an image!` }
     } else if (received_message.attachments) {
