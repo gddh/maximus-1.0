14690c3824324b3d345008513a4cca699a93629d
Derrick <d.lin168@berkeley.edu>
2018-05-13 21:21:45 -0700

    tracking

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get('/webhook', (req, res) => {
 
 const firstEntityValue = (entities, entity) => {
     const val = entities && entities[entity] && Array.isArray(entities[entity]) &&
-        entities[entity].length > 0 && entities[entity];
+        entities[entity].length > 0 && entities[entity][0];
     console.log("in first entity value");
     if (!val) {
         return null;
@@ -121,7 +121,7 @@ function handleMessage(sender_psid, received_message) {
             const pizza = firstEntityValue(entities, 'pizza_type');
             console.log("here:");
             console.log(entities);
-            console.log(pizza);
+            console.log(entities['intent']);
             if (pizza)
             {
                 response = {"text":  `Ok we will order your ${pizza.value} pizza`};
