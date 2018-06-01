b7e4cabb561e8f83eaa1ac70c628dbe4152f96d5
Derrick <d.lin168@berkeley.edu>
2018-06-01 10:20:17 -0700

    trying requirements.txt

--- emoji/response/score_texts_emojis.py
+++ emoji/response/score_texts_emojis.py
@@ -38,42 +38,42 @@ def read_in():
 TEST_SENTENCES = read_in()
 #TEST_SENTENCES = ['I love how you never reply back..']
 
-print('test sentences are: ' + TEST_SENTENCES)
+#print('test sentences are: ' + TEST_SENTENCES)
 
-#def top_elements(array, k):
-#    ind = np.argpartition(array, -k)[-k:]
-#    return ind[np.argsort(array[ind])][::-1]
-#
-#maxlen = 30
-#
-##print('Tokenizing using dictionary from {}'.format(VOCAB_PATH))
-#with open(VOCAB_PATH, 'r') as f:
-#    vocabulary = json.load(f)
-#
-#st = SentenceTokenizer(vocabulary, maxlen)
-#
-##print('Loading model from {}.'.format(PRETRAINED_PATH))
-#model = torchmoji_emojis(PRETRAINED_PATH)
-##print('Running predictions.')
-#tokenized, _, _ = st.tokenize_sentences(TEST_SENTENCES)
-#prob = model(tokenized)
-#for prob in [prob]:
-#    # Find top emojis for each sentence. Emoji ids (0-63)
-#    # correspond to the mapping in emoji_overview.png
-#    # at the root of the torchMoji repo.
-#    #print('Writing results to {}'.format(OUTPUT_PATH))
-#    scores = []
-#    for i, t in enumerate(TEST_SENTENCES):
-#        t_tokens = tokenized[i]
-#        t_score = []
-#        t_prob = prob[i]
-#        ind_top = top_elements(t_prob, 5)
-#        t_score.append(sum(t_prob[ind_top]))
-#        t_score.extend(ind_top)
-#        t_score.extend([t_prob[ind] for ind in ind_top])
-#        scores.append(t_score)
-#        print(t_score)
-#
+def top_elements(array, k):
+    ind = np.argpartition(array, -k)[-k:]
+    return ind[np.argsort(array[ind])][::-1]
+
+maxlen = 30
+
+#print('Tokenizing using dictionary from {}'.format(VOCAB_PATH))
+with open(VOCAB_PATH, 'r') as f:
+    vocabulary = json.load(f)
+
+st = SentenceTokenizer(vocabulary, maxlen)
+
+#print('Loading model from {}.'.format(PRETRAINED_PATH))
+model = torchmoji_emojis(PRETRAINED_PATH)
+#print('Running predictions.')
+tokenized, _, _ = st.tokenize_sentences(TEST_SENTENCES)
+prob = model(tokenized)
+for prob in [prob]:
+    # Find top emojis for each sentence. Emoji ids (0-63)
+    # correspond to the mapping in emoji_overview.png
+    # at the root of the torchMoji repo.
+    #print('Writing results to {}'.format(OUTPUT_PATH))
+    scores = []
+    for i, t in enumerate(TEST_SENTENCES):
+        t_tokens = tokenized[i]
+        t_score = []
+        t_prob = prob[i]
+        ind_top = top_elements(t_prob, 5)
+        t_score.append(sum(t_prob[ind_top]))
+        t_score.extend(ind_top)
+        t_score.extend([t_prob[ind] for ind in ind_top])
+        scores.append(t_score)
+        print(t_score)
+
 #    with open(OUTPUT_PATH, 'w') as csvfile:
 #        writer = csv.writer(csvfile, delimiter=str(','), lineterminator='\n')
 #        writer.writerow(['Text', 'Top5%',
